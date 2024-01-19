d71573d890d7aec53d3ce6e60a560a6176944644
harshithharsh <asharshith@gmail.com>
2024-01-19 18:53:57 +0800

    Initial commit

---
+++ src/test/java/com/example/cams/CamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
